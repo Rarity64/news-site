a97dba2e9b63bbd9a811e278931490781a0c0710
EduardSuyargulov <edsuyargulov@gmail.com>
2026-01-09 14:22:05 +0500

    reg added

--- news_site/news_site_app/urls.py
+++ news_site/news_site_app/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Главная страница
     path('', views.index, name='index'),
+    
+    #Страница авторизации
     path('auth/', views.auth, name='auth'), # name - это имя маршрута
+
+    #Страница регистрации
     path('reg/', views.reg, name='reg'),
 ]
 
